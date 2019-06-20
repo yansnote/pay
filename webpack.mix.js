f2eb1641fa986c4873703390b1daf622e3b4d233
Yan Naing <tannaing@gmail.com>
2019-06-20 15:46:28 +0630

    Make index html file

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/');
+mix.js('src/script/app.js', 'dist/').sass('src/style/app.scss', 'dist/');
 
 // Full API
 // mix.js(src, output);
