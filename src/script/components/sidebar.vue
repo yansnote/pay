ae11f61b038d9e699dbf8006447768433d93931d
Yan Naing <tannaing@gmail.com>
2019-08-27 15:33:26 +0630

    Use svg for sidebar icons

--- src/script/components/sidebar.vue
+++ src/script/components/sidebar.vue
@@ -11,32 +11,32 @@
                 </header> <!-- end of sidebar-title -->
 
                 <nav class="navigation">
-                    <ul>
+                    
                         <li>
-                            <a href="notes.html" class="d-flex active">
-                                <i class="material-icons mr-3">create</i>
+                            <a href="notes.html" class="d-flex align-items-center active">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" class="mr-2"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM21.41 6.34l-3.75-3.75-2.53 2.54 3.75 3.75 2.53-2.54z"/></svg>
                                 Notes
                             </a>
                         </li>
                         <li>
-                            <a href="tags.html" class="d-flex">
-                                <i class="material-icons mr-3">local_offer</i>
+                            <a href="tags.html" class="d-flex align-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" class="mr-2"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M22.83 12.99L11.83 2H2v9.83l10.99 10.99 9.84-9.83zM5.5 7C4.67 7 4 6.33 4 5.5S4.67 4 5.5 4 7 4.67 7 5.5 6.33 7 5.5 7z"/></svg>
                                 Tags
                             </a>
                         </li>
                         <li>
-                            <a href="profile.html" class="d-flex">
-                                <i class="material-icons mr-3">account_circle</i>
+                            <a href="profile.html" class="d-flex align-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" class="mr-2"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"/></svg>
                                 Profile
                             </a>
                         </li>
                         <li>
-                            <a href="settings.html" class="d-flex">
-                                <i class="material-icons mr-3">settings</i>
+                            <a href="settings.html" class="d-flex align-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" class="mr-2"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M3 17v2h6v-2H3zM3 5v2h10V5H3zm10 16v-2h8v-2h-8v-2h-2v6h2zM7 9v2H3v2h4v2h2V9H7zm14 4v-2H11v2h10zm-6-4h2V7h4V5h-4V3h-2v6z"/></svg>
                                 Settings
                             </a>
                         </li>
-                    </ul>
+                    
                 </nav>
 
                 <div id="sidebar-profile">
